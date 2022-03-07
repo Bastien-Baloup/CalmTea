b17add50640c12e18738eff5a4e758a4c2b34625
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-03-08 00:37:41 +0100

    Add and configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'peppermint': '#c5eada',
+        'castro': '#3f1d2b',
+        'beige': '#efedd5'
+      }
+    },
+  },
+  plugins: [],
+}
