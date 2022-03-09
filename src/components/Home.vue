32bb3f9f2f6e8811396d277498a03954388f7f5a
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-03-09 03:19:54 +0100

    Complete Home section text + adjusting visuals

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div id="hero" class="w-screen mt-20 bg-beige">
-    <img class="h-full w-full max-w-screen object-cover" src="/pattern.png" alt />
+  <!-- hero -->
+  <div id="hero" class="w-full mt-20 bg-beige scroll-mt-20">
+    <img class="h-full w-auto max-w-full m-auto max-w-screen object-cover" src="/pattern.png" alt />
   </div>
-  <div id="home" class="flex flex-col items-center justify-evenly w-full bg-peppermint">
+  <!-- home section -->
+  <section
+    id="home"
+    class="flex flex-col items-center justify-evenly w-full bg-peppermint scroll-mt-20"
+  >
     <img class="w-1/2" src="/logo-textless_castro.png" alt />
     <p class="text-castro text-2xl text-center px-10">
       <strong class="font-bold text-3xl">Calm Tea</strong> is a fresh beverage specialy crafted for a relaxing moment.
-      <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi laudantium quis, inventore debitis aliquam impedit neque maiores, voluptas repellendus perferendis eius provident pariatur incidunt? Mollitia sapiente quasi hic expedita eos.
+      <br />Simply open the can, lay back, and enjoy the taste of white tea, peppermint, jasmine and chamomile.
     </p>
-  </div>
+  </section>
 </template>
 
 <script setup>
@@ -17,6 +22,6 @@
 <style lang="postcss">
 #hero,
 #home {
-  height: calc(100vh - 5rem);
+  height: calc(100vh - 5rem); /* viewport height - header height */
 }
 </style>
